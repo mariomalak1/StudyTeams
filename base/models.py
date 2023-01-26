cc05a2f8f43163c63fc4d28c25981c281ad9ddce
mario malak <93185168+mariomalak1@users.noreply.github.com>
2023-01-26 02:26:27 +0200

    making first three models, urls)

--- base/models.py
+++ base/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+    
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete= models.SET_NULL, null = True)
+    createdDateTime = models.DateTimeField(auto_now_add=True)
+    updatedDateTime = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    topic = models.ForeignKey("Topic", on_delete=models.CASCADE, null = True) 
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    content = models.TextField()
+    createdTime = models.DateTimeField(auto_now_add=True)    
+    room = models.ForeignKey('Room', on_delete=models.CASCADE)
+    
+    
+    def __str__(self):
+        return self.content[0:7]
+    
+
